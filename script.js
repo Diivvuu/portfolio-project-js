2254546cf571ffaed104916ebcf6b9f7d9f85fb6
Diivvuu <divyanshubawa9@gmail.com>
2024-04-09 01:30:26 +0530

    completed

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ let banner = document.querySelector(".banner")
 dayNight.addEventListener("click", ()=>{
     banner.classList.toggle("night")
 })
+
+let typingEffect = new Typed("#text", {
+    strings:["an UnderGraduate", "Coder", "GymRat"], 
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 500,
+})
